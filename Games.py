5e020b8526cb7ba92fdd5f8d415ef604d458238d
NoaLeGeek <noacraft68@gmail.com>
2022-05-14 14:12:12 +0200

    Updates: - Modes created for guess the number

--- Games.py
+++ Games.py
@@ -1,16 +1,53 @@
 import random
+
 import Utils
 
 
 # Guess the number
-def guess_number(minimum: int = 1, maximum: int = 100, tries: int = 3) -> None:
-    if minimum == maximum:
-        print("The minimum and maximum values should not be the same.")
-    else:
-        if minimum > maximum:
-            swap = maximum
-            maximum = minimum
-            minimum = swap
+def guess_number() -> None:
+    minimum = 0
+    maximum = 0
+    tries = 0
+    menu = {0: "CUSTOM MODE: Guess the number with your settings",
+            1: "CUSTOM MODE WITH TRIES: Guess the number with your settings and your tries",
+            2: "1MILLION MODE: Guess the number between 1 and 1.000.000",
+            3: "1BILLION MODE: Guess the number between 1, and 1.000.000.000",
+            4: "1MILLION MODE WITH TRIES: Guess the number between 1 and 1.000.000 with 30 tries",
+            5: "1BILLION MODE WITH TRIES: Guess the number between 1, and 1.000.000.000 with 45 tries"}
+    match (int(input("Choose your mode!\n" + Utils.generate_choices(menu)))):
+        case 0:
+            minimum = int(input("I'm thinking of a number between [?] and ... included."))
+            maximum = int(input("I'm thinking of a number between" + str(minimum) + "and ... included."))
+            if minimum == maximum:
+                print("The minimum and maximum values should not be the same.")
+                return
+        case 1:
+            minimum = int(input("I'm thinking of a number between [?] and ... included. You have ... tries."))
+            maximum = int(input("I'm thinking of a number between " + str(minimum) + " and [?] included."))
+            tries = int(input("I'm thinking of a number between " + str(minimum) + " and " + str(
+                maximum) + "included. You have [?] tries."))
+            if minimum == maximum:
+                print("The minimum and maximum values should not be the same.")
+                return
+        case 2:
+            minimum = 1
+            maximum = 1000000
+        case 3:
+            minimum = 1
+            maximum = 1000000000
+        case 4:
+            minimum = 1
+            maximum = 1000000
+            tries = 30
+        case 5:
+            minimum = 1
+            maximum = 1000000000
+            tries = 45
+    if minimum > maximum:
+        swap = maximum
+        maximum = minimum
+        minimum = swap
+    if tries != 0:
         number = random.randint(minimum, maximum)
         guess = maximum + 1
         print("I'm thinking of a number between", minimum, "and", str(maximum) + ". You have", tries, "tries.")
@@ -27,14 +64,27 @@ def guess_number(minimum: int = 1, maximum: int = 100, tries: int = 3) -> None:
             elif guess > number:
                 print("It's less!")
             tries -= 1
+    else:
+        number = random.randint(minimum, maximum)
+        guess = maximum + 1
+        print("I'm thinking of a number between", minimum, "and", str(maximum) + ".")
+        while guess != number:
+            guess = int(input("Guess the number!\n"))
+            if guess == number:
+                print("You guessed it!")
+                break
+            elif guess < number:
+                print("It's more!")
+            elif guess > number:
+                print("It's less!")
 
 
 # Plays rock, paper, scissors
 def rock_paper_scissors() -> None:
-    lizt = {0: "Rock", 1: "Paper", 2: "Scissors"}
+    choices = {0: "Rock", 1: "Paper", 2: "Scissors"}
     choice = 3
     while choice > 2 or choice < 0:
-        choice = int(input("Rock, Paper, Scissors: Choose!\n" + Utils.generate_choices(lizt)))
+        choice = int(input("Rock, Paper, Scissors: Choose!\n" + Utils.generate_choices(choices)))
         if choice < 3:
             break
         print("Invalid choice. You can only choose 0, 1 or 2.")
@@ -45,11 +95,15 @@ def rock_paper_scissors() -> None:
             rock_paper_scissors()
     # You win
     elif (choice == 0 and computer == 2) or (choice == 1 and computer == 0) or (choice == 2 and computer == 1):
-        replay = int(input("The computer chose " + lizt[computer] + "\nYou win!\nWants to replay?\n" + Utils.generate_choices({0: "Yes", 1: "No"})))
+        replay = int(input(
+            "The computer chose " + choices[computer] + "\nYou win!\nWants to replay?\n" + Utils.generate_choices(
+                {0: "Yes", 1: "No"})))
         if replay == 0:
             rock_paper_scissors()
     # You lose
     else:
-        replay = int(input("The computer chose " + lizt[computer] + "\nYou lose!\nWants to replay?\n" + Utils.generate_choices({0: "Yes", 1: "No"})))
+        replay = int(input(
+            "The computer chose " + choices[computer] + "\nYou lose!\nWants to replay?\n" + Utils.generate_choices(
+                {0: "Yes", 1: "No"})))
         if replay == 0:
             rock_paper_scissors()
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from random import randint
+
 import Polygon
 import Utils
 import Games
 import MathsUtils
+import math
 
 
 def main():
-    Games.rock_paper_scissors()
+    x = 0
+    y = randint(0, 100)
+    while y != 1:
+        y = math.floor(y / 2)
+        print(y)
+        x += 1
+    print(x)
+    print(x * 1.5)
+    Games.guess_number()
 
 
 if __name__ == '__main__':
